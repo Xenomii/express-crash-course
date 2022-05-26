4aad75c8023f68ac6d5502b3f3f41388bb659dec
Xenomii <voltzxp@gmail.com>
2022-05-26 12:57:34 +0800

    update

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express()
 
-app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-    console.log('Here')
-    res.render('index', { text: 'World' })
-})
+app.set('view engine', 'ejs')
 
 const userRouter = require('./routes/users')
 
 app.use('/users', userRouter)
+
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
 
 app.listen(3000)
